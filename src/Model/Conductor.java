8f152369f72688ea002f7f05037fc537120ce0f7
Andres Felipe Zabaleta Diaz <afzabaleta@gmail.com>
2026-03-14 16:24:35 -0500

    feat: Se implemento encapsulamiento en la clase Conductor

--- src/Model/Conductor.java
+++ src/Model/Conductor.java
@@ -3,4 +3,35 @@ package Model;
 public class Conductor extends Persona{
     private String numLicencia;
     private String CateLicencia;
+
+    public Conductor(String cedula, String nombre, String cateLicencia, String numLicencia) {
+        super(cedula, nombre);
+        CateLicencia = cateLicencia;
+        this.numLicencia = numLicencia;
+    }
+
+    public String getCateLicencia() {
+        return CateLicencia;
+    }
+
+    public void setCateLicencia(String cateLicencia) {
+        CateLicencia = cateLicencia;
+    }
+
+    public String getNumLicencia() {
+        return numLicencia;
+    }
+
+    public void setNumLicencia(String numLicencia) {
+        this.numLicencia = numLicencia;
+    }
+
+    public boolean tieneLicencia(){
+        return false;
+    }
+
+    @Override
+    public void imprimirDetalle() {
+
+    }
 }
