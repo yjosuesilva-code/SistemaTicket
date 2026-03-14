25d55602bb22678559099eef37d50c7b09b01b20
Andres Felipe Zabaleta Diaz <afzabaleta@gmail.com>
2026-03-14 16:16:22 -0500

    feat: Se implemento encapsulamiento en la clase Pasajero

--- src/Model/Pasajero.java
+++ src/Model/Pasajero.java
@@ -2,4 +2,24 @@ package Model;
 
 public abstract class Pasajero extends Persona {
     private String tipoPasajero;
+
+    public Pasajero(String cedula, String nombre, String tipoPasajero) {
+        super(cedula, nombre);
+        this.tipoPasajero = tipoPasajero;
+    }
+
+    public String getTipoPasajero() {
+        return tipoPasajero;
+    }
+
+    public void setTipoPasajero(String tipoPasajero) {
+        this.tipoPasajero = tipoPasajero;
+    }
+
+    @Override
+    public abstract void imprimirDetalle();
+
+    public abstract double calcularDescuento();
+
+
 }
