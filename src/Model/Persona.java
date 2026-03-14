44472dd06e815854e38033066f0103d5cbbdcbc1
Andres Felipe Zabaleta Diaz <afzabaleta@gmail.com>
2026-03-14 16:08:47 -0500

    feat: Se implemento encapsulamiento en la clase Persona

--- src/Model/Persona.java
+++ src/Model/Persona.java
@@ -4,4 +4,26 @@ public abstract class Persona {
     private String nombre;
     private String cedula;
 
+    public Persona(String cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public abstract void imprimirDetalle();
+
 }
