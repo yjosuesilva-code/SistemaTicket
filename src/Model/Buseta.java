66daebef963a3f8f296d7b75346d933d3501a39b
k-sayago <ksayago@unicesar.edu.co>
2026-03-14 15:16:11 -0500

    feat: se implemento constructor en la clase buseta

--- src/Model/Buseta.java
+++ src/Model/Buseta.java
@@ -2,5 +2,9 @@ package Model;
 
 public class Buseta extends Vehiculo{
     protected static final int CAPACIDAD_MAX=19;
-    protected static final double TARIFA_BASE=8000;{
+    protected static final double TARIFA_BASE=8000;
+
+    public Buseta(String placa, String ruta) {
+        super(placa, ruta, 19, 0, true, 8000);
+    }
 }
