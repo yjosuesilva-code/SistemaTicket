f8a86f45a8c64c4600c17244cbe15f9f2665bb41
Andres Felipe Zabaleta Diaz <afzabaleta@gmail.com>
2026-03-14 17:05:05 -0500

    feat: Se implemento encapsulamiento en la clase Ticket

--- src/Model/Ticket.java
+++ src/Model/Ticket.java
@@ -7,4 +7,57 @@ public class Ticket {
     private String origen;
     private String destino;
     private double valorFinal;
+    public Ticket(Pasajero pasajero, Vehiculo vehiculo, String origen, String destino) {
+        this.pasajero = pasajero;
+        this.vehiculo = vehiculo;
+        this.origen = origen;
+        this.destino = destino;
+        this.fechaCompra= LocalDate.now();
+        this.valorFinal = calcularTotal();
+    }
+
+    public Pasajero getPasajero() {
+        return pasajero;
+    }
+
+    public void setPasajero(Pasajero pasajero) {
+        this.pasajero = pasajero;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public LocalDate getFechaCompra() {
+        return fechaCompra;
+    }
+
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public double getValorFinal() {
+        return valorFinal;
+    }
+
+    public void setValorFinal(double valorFinal) {
+        this.valorFinal = valorFinal;
+    }
 }
