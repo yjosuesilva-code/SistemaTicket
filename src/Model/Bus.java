d97a9db34df0e443333a731fb8ae4c50403aa3bd
k-sayago <ksayago@unicesar.edu.co>
2026-03-14 15:01:55 -0500

    feat: se implemento constructor en la clase bus

--- src/Model/Bus.java
+++ src/Model/Bus.java
@@ -3,4 +3,8 @@ package Model;
 public class Bus extends Vehiculo {
     protected static final int CAPACIDAD_MAX = 45;
     protected static final double TARIFA_BASE = 15000.0;
+
+    public Bus(String placa, String ruta) {
+        super(placa, ruta, 45, 0, true, 15000);
+    }
 }
