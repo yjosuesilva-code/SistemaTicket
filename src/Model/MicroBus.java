deafd5fd173d5cfc52f10d81dfafdb7ae2ff0bd7
k-sayago <ksayago@unicesar.edu.co>
2026-03-14 15:28:55 -0500

    feat: se implemento constructor en la clase MicroBus

--- src/Model/MicroBus.java
+++ src/Model/MicroBus.java
@@ -3,4 +3,8 @@ package Model;
 public class MicroBus extends Vehiculo {
     protected static final int CAPACIDAD_MAX = 25;
     protected static final double TARIFA_BASE = 10000.0;
+
+    public MicroBus(String placa, String ruta) {
+        super(placa, ruta, 25, 0, true, 10000);
+    }
 }
