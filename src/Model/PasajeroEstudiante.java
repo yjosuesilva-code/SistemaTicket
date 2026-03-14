8a0689530bacb8f8f68e7386ebb80fe583739bd3
Andres Felipe Zabaleta Diaz <afzabaleta@gmail.com>
2026-03-14 16:40:10 -0500

    feat: Se implemento encapsulamiento en la clase PasajeroEstudiante

--- src/Model/PasajeroEstudiante.java
+++ src/Model/PasajeroEstudiante.java
@@ -1,5 +1,17 @@
 package Model;
 
 public class PasajeroEstudiante extends Pasajero{
+    public PasajeroEstudiante(String cedula, String nombre) {
+        super(cedula, nombre, "Estudiante");
+    }
+
+    @Override
+    public double calcularDescuento() {
+        return 0.15;
+    }
+    @Override
+    public void imprimirDetalle() {
+
+    }
 
 }
