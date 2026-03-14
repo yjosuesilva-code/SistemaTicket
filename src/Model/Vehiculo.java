feaa5882a190b8cffa416b2f5a40205bfb3828db
k-sayago <ksayago@unicesar.edu.co>
2026-03-14 14:45:54 -0500

    feat:implementacion de la clase Abstracta Vehiculo en Model con atributos

---
+++ src/Model/Vehiculo.java
@@ -0,0 +1,10 @@
+package Model;
+
+public abstract class Vehiculo {
+    private String placa;
+    private String ruta;
+    private int capacidadMaxima;
+    private int contadorPasajeros;
+    private boolean disponible;
+    private double tarifaBase;
+}
