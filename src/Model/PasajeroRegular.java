7e00a0126dfd6281029afde1272fda2250500759
Andres Felipe Zabaleta Diaz <afzabaleta@gmail.com>
2026-03-14 16:34:03 -0500

    feat: Se implemento encapsulamiento en la clase PasajeroRegular

--- src/Model/PasajeroRegular.java
+++ src/Model/PasajeroRegular.java
@@ -2,4 +2,16 @@ package Model;
 
 public class PasajeroRegular extends Pasajero{
 
+    public PasajeroRegular(String cedula, String nombre) {
+        super(cedula, nombre, "Regular");
+    }
+    @Override
+    public double calcularDescuento(){
+        return 0.0;
+    }
+
+    @Override
+    public void imprimirDetalle() {
+
+    }
 }
