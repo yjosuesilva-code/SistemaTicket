61624c535082649f0d2517c03c791366e0ac8cd8
Andres Felipe Zabaleta Diaz <afzabaleta@gmail.com>
2026-03-14 17:00:17 -0500

    feat: Se creo la clase Ticket y sus atributos

---
+++ src/Model/Ticket.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Ticket {
+    private Pasajero pasajero;
+    private Vehiculo vehiculo;
+    private LocalDate fechaCompra;
+    private String origen;
+    private String destino;
+    private double valorFinal;
+}
