ea001982cf9dd541178b382cc633dba4974a5790
k-sayago <ksayago@unicesar.edu.co>
2026-03-16 15:17:12 -0500

    feat: se implemento el metodo registrarVehiculo en la clase VehiculoService

--- src/Service/VehiculoService.java
+++ src/Service/VehiculoService.java
@@ -1,6 +1,7 @@
 package Service;
 
 import Dao.VehiculoDao;
+import Model.Vehiculo;
 
 public class VehiculoService {
 
@@ -8,5 +9,15 @@ public class VehiculoService {
 
     public VehiculoService(VehiculoDao vehiculoDao) {
         this.vehiculoDao = vehiculoDao;
+    }
+    public boolean registrarVehiculo(Vehiculo v) {
+        if (placaExiste(v.getPlaca())) {
+            System.out.println("[VehiculoService] Error: ya existe un vehículo con placa " + v.getPlaca());
+            return false;
+        }
+        vehiculoDao.guardar(v);
+        System.out.println("[VehiculoService] Vehículo registrado: " + v.getPlaca()
+                + " | Ruta: " + v.getRuta());
+        return true;
     }
 }
