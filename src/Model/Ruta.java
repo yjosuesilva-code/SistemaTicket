4d447fea51fe9e8c5a217616fbd167335998038e
k-sayago <ksayago@unicesar.edu.co>
2026-03-18 17:22:16 -0500

    feat: se creo la clase Ruta con sus atributos

---
+++ src/Model/Ruta.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Ruta {
+    private String codigo;
+    private String ciudadOrigen;
+    private String ciudadDestino;
+    private double distanciaKm;
+    private int tiempoEstimadoMin;
+}
