9d5c0e747b12d93e7f86a4b8cd20df6b0c2e1bf7
Andres Felipe Zabaleta Diaz <afzabaleta@gmail.com>
2026-03-21 15:26:33 -0500

    feat: se implemento el arrayList y el metodo guardar en la clase ReservaDao

--- src/Dao/ReservaDao.java
+++ src/Dao/ReservaDao.java
@@ -8,4 +8,13 @@ public class ReservaDao {
     private static final String RUTA_ARCHIVO = "data/reservas.txt";
     private static final int    CAMPOS       = 12;
     private List<Reserva> lista;
+
+    public ReservaDao() {
+        lista = new ArrayList<>();
+        cargarTodos();
+    }
+    public void guardar(Reserva r) {
+        lista.add(r);
+        escribirLinea(r);
+    }
 }
