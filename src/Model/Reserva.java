d7336e1b9e75e794da3e751b7df1a690fc3ebd30
Andres Felipe Zabaleta Diaz <afzabaleta@gmail.com>
2026-03-21 14:57:45 -0500

    feat: se creo los constructores de la clase Reserva

--- src/Model/Reserva.java
+++ src/Model/Reserva.java
@@ -1,11 +1,34 @@
 package Model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Reserva {
     public enum Estado { ACTIVA, CONVERTIDA, CANCELADA }
     private String        codigo;
     private Pasajero      pasajero;
     private Vehiculo      vehiculo;
     private LocalDateTime fechaCreacion;
-    private LocalDate     fechaViaje;
+    private LocalDate fechaViaje;
     private Estado        estado;
+
+    public Reserva(String codigo, Pasajero pasajero, Vehiculo vehiculo, LocalDate fechaViaje) {
+        this.codigo        = codigo;
+        this.pasajero      = pasajero;
+        this.vehiculo      = vehiculo;
+        this.fechaCreacion = LocalDateTime.now();
+        this.fechaViaje    = fechaViaje;
+        this.estado        = Estado.ACTIVA;
+    }
+
+    public Reserva(String codigo, Pasajero pasajero, Vehiculo vehiculo,
+                   LocalDateTime fechaCreacion, LocalDate fechaViaje, Estado estado) {
+        this.codigo        = codigo;
+        this.pasajero      = pasajero;
+        this.vehiculo      = vehiculo;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaViaje    = fechaViaje;
+        this.estado        = estado;
+    }
+
 }
